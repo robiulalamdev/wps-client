aec8dc07bbddc09d24eb1b11c39b868b0780c221
robiulalamdev <robiulalam762400@gmail.com>
2024-10-09 13:26:08 +0600

    meta tags add and pwa test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
+import nextPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{ hostname: "api.thewallpapersociety.com" }],
+    remotePatterns: [
+      {
+        hostname: "api.thewallpapersociety.com",
+      },
+    ],
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
