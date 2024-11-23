f3e56d76ad3b81976a96f4f7d81125bf4752112b
robiulalamdev <robiulalam762400@gmail.com>
2024-11-23 22:18:12 +0600

    "Updated Tailwind config to remove unnecessary comment and adjusted file paths for Material Tailwind React components"

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,9 @@ module.exports = withMT({
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
