ea9e0397b79ac43513b90b81957c2f0e30269b68
robiulalamdev <robiulalam762400@gmail.com>
2025-01-04 15:48:36 +0600

    feat: add dashboard and home icons to HeaderProfile component

--- src/components/shared/headers/HeaderProfile.jsx
+++ src/components/shared/headers/HeaderProfile.jsx
@@ -7,6 +7,7 @@ import {
 } from "@material-tailwind/react";
 
 import {
+  IGoToDashboard,
   iHLove,
   iLock,
   iLogout,
@@ -18,7 +19,7 @@ import { AuthContext } from "../../../contextApi/AuthContext";
 import useViewImage from "../../../lib/hooks/useViewImage";
 import { DefaultProfile } from "../../../lib/data/globalData";
 import { useRouter } from "next/router";
-import Image from "next/image";
+import { ROLE_DATA } from "@/lib/config";
 const HeaderProfile = () => {
   const { viewResizeImg } = useViewImage();
   const { logout, user } = useContext(AuthContext);
@@ -27,64 +28,75 @@ const HeaderProfile = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div onMouseLeave={() => setOpen(false)}>
-      <SpeedDial placement="left" open={open}>
-        <SpeedDialHandler onMouseEnter={() => setOpen(true)}>
-          <div
-            className={`rounded-full cursor-pointer w-[62px] h-[62px] flex justify-center items-center  backdrop-blur-lg ${
-              open ? "bg-[#00000050] rounded-l-none" : "bg-[#00000033]"
-            }`}
-          >
-            <img
-              src={
-                viewResizeImg(user?.profile?.profile_image, 55, 55) ||
-                DefaultProfile.src
-              }
-              alt="profile"
-              className="w-[50px] h-[50px] rounded-full object-cover"
-            />
-          </div>
-        </SpeedDialHandler>
-        {open && (
-          <SpeedDialContent
-            onMouseEnter={() => setOpen(true)}
-            className={`w-[275px] h-[62px] backdrop-blur-lg rounded-[100px] rounded-r-none ml-[5px] flex flex-row justify-start items-center gap-x-[10px] pl-[25px] ${
-              open ? "bg-[#00000050]" : "bg-[#00000033]"
-            }`}
-          >
-            <IconButton
-              onClick={() => logout()}
-              className="p-0 rounded-full bg-transparent hover:bg-black"
+    <div className="flex items-center gap-x-[11px]">
+      {(user?.role === ROLE_DATA.ADMIN || user?.role === ROLE_DATA.MOD) && (
+        <IconButton
+          onClick={() => router.push("/dashboard")}
+          className="w-[37px] h-[34px] p-0 bg-[#00000033] text-white hover:text-[#FF001F]"
+        >
+          {IGoToDashboard}
+        </IconButton>
+      )}
+
+      <div onMouseLeave={() => setOpen(false)}>
+        <SpeedDial placement="left" open={open}>
+          <SpeedDialHandler onMouseEnter={() => setOpen(true)}>
+            <div
+              className={`rounded-full cursor-pointer w-[62px] h-[62px] flex justify-center items-center  backdrop-blur-lg ${
+                open ? "bg-[#00000050] rounded-l-none" : "bg-[#00000033]"
+              }`}
             >
-              {iLogout}
-            </IconButton>
-            <IconButton
-              onClick={() => router.push("/account-settings")}
-              className="p-0 rounded-full bg-transparent hover:bg-black"
+              <img
+                src={
+                  viewResizeImg(user?.profile?.profile_image, 55, 55) ||
+                  DefaultProfile.src
+                }
+                alt="profile"
+                className="w-[50px] h-[50px] rounded-full object-cover"
+              />
+            </div>
+          </SpeedDialHandler>
+          {open && (
+            <SpeedDialContent
+              onMouseEnter={() => setOpen(true)}
+              className={`w-[275px] h-[62px] backdrop-blur-lg rounded-[100px] rounded-r-none ml-[5px] flex flex-row justify-start items-center gap-x-[10px] pl-[25px] ${
+                open ? "bg-[#00000050]" : "bg-[#00000033]"
+              }`}
             >
-              {iSetting}
-            </IconButton>
-            <IconButton
-              onClick={() => router.push("/vault")}
-              className="p-0 rounded-full bg-transparent hover:bg-black"
-            >
-              {iLock}
-            </IconButton>
-            <IconButton
-              onClick={() => router.push("/media-center/favorites")}
-              className="p-0 rounded-full bg-transparent hover:bg-black"
-            >
-              {iHLove}
-            </IconButton>
-            <IconButton
-              onClick={() => router.push("/my-profile")}
-              className="p-0 rounded-full bg-transparent hover:bg-black"
-            >
-              {iUser}
-            </IconButton>
-          </SpeedDialContent>
-        )}
-      </SpeedDial>
+              <IconButton
+                onClick={() => logout()}
+                className="p-0 rounded-full bg-transparent hover:bg-black"
+              >
+                {iLogout}
+              </IconButton>
+              <IconButton
+                onClick={() => router.push("/account-settings")}
+                className="p-0 rounded-full bg-transparent hover:bg-black"
+              >
+                {iSetting}
+              </IconButton>
+              <IconButton
+                onClick={() => router.push("/vault")}
+                className="p-0 rounded-full bg-transparent hover:bg-black"
+              >
+                {iLock}
+              </IconButton>
+              <IconButton
+                onClick={() => router.push("/media-center/favorites")}
+                className="p-0 rounded-full bg-transparent hover:bg-black"
+              >
+                {iHLove}
+              </IconButton>
+              <IconButton
+                onClick={() => router.push("/my-profile")}
+                className="p-0 rounded-full bg-transparent hover:bg-black"
+              >
+                {iUser}
+              </IconButton>
+            </SpeedDialContent>
+          )}
+        </SpeedDial>
+      </div>
     </div>
   );
 };
