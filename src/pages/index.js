bd944619cc36806878dd24c6dd690dea7998bcb2
robiulalamdev <robiulalam762400@gmail.com>
2025-01-09 14:37:32 +0600

    feat: update environment variables and enhance SEO with MetaTags component in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,23 @@ import WPSStaffPicks from "../components/home/WPSStaffPicks/WPSStaffPicks";
 import Banner from "../components/home/banner/Banner";
 import OfficialWallpapers from "../components/home/officialWallpapers/OfficialWallpapers";
 import MainHeader from "../components/shared/headers/MainHeader";
+import MetaTags from "@/components/common/SEO/MetaTags";
+import useViewImage from "@/lib/hooks/useViewImage";
+import { CLIENT_URL } from "@/lib/config";
 
 export default function Home() {
+  const { viewResizeImg } = useViewImage();
   return (
     <>
+      <MetaTags
+        title="WPS - Free 4K/HD Wallpapers, Ad-Free. Join the Society."
+        description="WPS - Free 4K/HD Wallpapers, Ad-Free. Join the Society. Explore this stunning Neon Retro wallpaper featuring vibrant purple ferns. Perfect for a high-definition setup!"
+        image={viewResizeImg("src/assets/images/card.jpg", 600, 600)}
+        url={CLIENT_URL}
+        // username=""
+        width={200}
+        height={200}
+      />
       <MainHeader />
       <Banner />
       <OfficialWallpapers />
